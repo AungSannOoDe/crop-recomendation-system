ca092dff4354bd70e8bc1e3ea24edc9ddb251daf
AungSannOoDe <theflash1321@gmail.com>
2026-01-31 23:09:06 +0630

    feat:add regster user

--- crop_app/urls.py
+++ crop_app/urls.py
@@ -3,8 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('login',login,name="login"),
-    path('sign-up',signup,name="signup")
+    path('login',login_view,name="login"),
+    path('signup/',signup,name="signup"),
+    path('predict',predict,name="prediction"),
+    path('logout',logout,name='logout')
     # You can add your prediction path here later
     # path('predict/', views.predict, name='predict'),
 ]
--- crop_app/views.py
+++ crop_app/views.py
@@ -1,10 +1,52 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
+from django.contrib import messages
+from django.contrib.auth import authenticate,logout
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.models import User
+from .models import UserProfile
 # Create your views here.
 def home(request):
     return render(request,"home.html")
 
-def login(request):
+def login_view(request):
     return  render(request,"login.html")
+def predict(request):
+    return render(request,"predict.html")
+def logout_view(request):
+    logout(request)
+    messages.success("user logout successfully")
+    return redirect("login")    
 def signup(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        if not name or not email or not password:
+            messages.error(request,"please fill required fields")
+            return redirect("signup")
+        if len(password) <6:
+           messages.error(request,"password should be at least 6  characters")
+           return redirect("signup") 
+        if password != confirm_password:
+           messages.error(request, "Passwords do not match.")
+           return redirect("signup")
+        if User.objects.filter(email=email).exists():
+           messages.error(request,"This email is already registered.")
+           return redirect("signup")  
+        try:
+           user=User.objects.create_user(
+            username=name,
+            email=email,
+            password=password
+           )  
+           user.save()
+           UserProfile.objects.create(user=user,email=email)
+           auth_login(request,user)
+           messages.success(request, "Account created! Please login.")
+           return redirect("prediction")
+        except Exception as e:
+          messages.error(request, "An error occurred. Please try again.")   
+          return redirect("signup") 
+
     return render(request,"signup.html")    
